7db916f6daa374d50c54b46f704bf30cfcaf9f53
Andrei Katona <katona.andrei@gmail.com>
2020-10-16 11:29:37 +0200

    Transitions added and small refactoring

--- gmltotapn.py
+++ gmltotapn.py
@@ -8,6 +8,7 @@ network = 'Arpanet196912'
 #Initialising the GML reader
 G = nx.read_gml(network + '.gml')
 nodes = list(G.nodes(data=True))
+edges = list(G.edges)
 
 #TAPAAL Window Canva size
 canvas_width = 3000
@@ -17,13 +18,23 @@ canvas_height = 1000
 labels = []
 lats = []
 longs = []
+transitions = []
+conv_lats = []
+conv_longs = []
 
 #Sepparating Id, Lat and Long
-i = 0
 for i in range (len(G.nodes)):
     labels.append(nodes[i][0]) #Label
     lats.append(nodes[i][1]['Latitude'])
     longs.append(nodes[i][1]['Longitude'])
+
+#Formulas used to convert gps coordinates to x,y based on
+#the size of your canvas
+def long_to_x(long):
+    return ((int)((canvas_width/360.0) * (180 + long) *5 - 2000))
+def lat_to_y(lat):
+    return ((int)((canvas_height/180.0) * (90 - lat) *5 - 1200))
+    
 
 
 #Writing to file
@@ -36,9 +47,24 @@ f.write("  <net active=\"true\" id=\"" + network + "\" type=\"P/T net\">\n")
 for i in range (len(G.nodes)): 
     #  Writing each node to file. Highly unoptimized/dirty/all that,
     #will be fixed and made more pretty.
-    f.write("    <place displayName=\"true\" id=\"" + labels[i] + "\" initialMarking=\"0\" invariant=\"&lt; inf\" name=\"" + labels[i] + "\" nameOffsetX=\"-5.0\" nameOffsetY=\"35.0\" positionX=\"" + str((int)((canvas_width/360.0) * (180 + longs[i]) *5 - 2000)) + "\" positionY=\"" + str((int)((canvas_height/180.0) * (90 - lats[i]) *5-1200)) + "\"/>\n")
+    f.write("    <place displayName=\"true\" id=\"" + labels[i] + "\" initialMarking=\"0\" invariant=\"&lt; inf\" name=\"" + labels[i] + "\" nameOffsetX=\"-5.0\" nameOffsetY=\"35.0\" positionX=\"" + str(long_to_x(longs[i])) + "\" positionY=\"" + str(lat_to_y(lats[i])) + "\"/>\n")
+
+#Generating transitions from edges, same remarks as above
+for i in range(len(G.edges(data=True))):
+    #Calculating the middle distance
+    x0 = long_to_x(G.nodes(data=True)[edges[i][0]]['Longitude'])
+    x1 = long_to_x(G.nodes(data=True)[edges[i][1]]['Longitude'])
+    y0 = lat_to_y(G.nodes(data=True)[edges[i][0]]['Latitude'])
+    y1 = lat_to_y(G.nodes(data=True)[edges[i][1]]['Latitude'])
+    x = (x0 + x1) / 2
+    y = (y0 + y1) / 2
+    f.write("    <transition angle=\"0\" displayName=\"true\" id=\"" + str(edges[i][0] + "_" + edges[i][1]) + "\" infiniteServer=\"false\" name=\"" + edges[i][0] + "_" + edges[i][1] + "\" nameOffsetX=\"-5.0\" nameOffsetY=\"35.0\" positionX=\"" + str(x) + "\" positionY=\"" + str(y) + "\" priority=\"0\" urgent=\"false\"/>\n")
+    
+
 
 f.write("  </net>\n")
 f.write("  <k-bound bound=\"3\"/>\n")
 f.write("</pnml>")
 f.close()
+
+print("Done! " + network + " converted to .tpn!")
