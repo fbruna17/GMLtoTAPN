66779a6b9f34b4feba2961351ba54d9b7fe59bfb
Andrei Katona <katona.andrei@gmail.com>
2020-10-20 05:53:19 +0200

    Added comments

--- gmltotapn_circle.py
+++ gmltotapn_circle.py
@@ -1,11 +1,21 @@
 import networkx as nx #Library used to work with GML files
 import math
 
-#GML Network name to be converted
-#Has to be in same directory as .py file for now
+### TODO:
+##  - !!!!change using node labels to just counting the nodes and
+# getting the transition rules from edges as some data sets do not have them
+#   - ?different spacing between transitions based on how many outgoings a node has
+#   - ?rotate somehow the transitions so that the lines don't seem to overlap
+##  - ~automatically change settings based on network size
+###########
+
+#GML Network to be converted
+#### HAS TO BE IN SAME DIRECTORY AS PY FILE FOR NOW
+## INPUT ITS NAME BELLOW
 #network = 'Arpanet196912'
-network = 'Aarnet'
+#network = 'Aarnet'
 #network = 'Aconet'
+network = 'Ai3'
 
 #Initialising the GML reader
 G = nx.read_gml(network + '.gml')
@@ -15,11 +25,12 @@ nodes_raw = list(G.nodes(data = True))
 edges_raw = list(G.edges)
 
 
-#x,y offset/center, spacing and ray size
-xs = 600
-ys = 600
-r = 500
-tspace = 100
+### SETTINGS ###
+xs = 600 #x offset
+ys = 600 #y offset
+r = 500  #circle ray, has to be smaller than offset
+tspace = 60 #spacing between transitions
+################
 
 #Needed variables
 nodes = []
@@ -29,27 +40,34 @@ transitions = []
 transitions_label = {}
 ntc = {}
 
+#Extracting information from GML
 for i in range(nodes_count):
     nodes.append(nodes_raw[i][0])
     nodes_label[nodes_raw[i][0]] = "P" + str(i)
     ntc["P" + str(i)] = 0
-
 for i in range(edges_count):
     source, target = nodes_label[edges_raw[i][0]], nodes_label[edges_raw[i][1]]
     transitions.append((source, target))
     transitions_label[(source, target)] = "T" + str(i)
 
-def rgen(x, ray):
-    alpha = 2 * math.pi * x / nodes_count
-    return (int(math.cos(alpha) * ray) + xs, int(math.sin(alpha) * ray) + ys)
+#Generates a set of (x,y) coordinates in the "nth slice"
+#of the circle at a certain radius
+def rgen(n, radius):
+    alpha = 2 * math.pi * n / nodes_count
+    return (int(math.cos(alpha) * radius) + xs, int(math.sin(alpha) * radius) + ys)
 
+#For each node, its circle coordinates are generated here
+#by using the method above
 for x in range(nodes_count):
     nodes_cords[nodes_label[nodes[x]]] = rgen(x, r)
 
-#Transition rotation
+#Calculates the angle between 2 points
 def slope(x0, x1, y0, y1):
     return (int(math.degrees(math.atan2((y1-y0),(x1-x0)))))
 
+#Generates the coordinate of a transition
+#Each node is tracked to see how many outputs it has
+#With each output, the radius of the circle is decreased
 def tran_locator(x, y, t):
     ntc[t] +=1
     a, b = rgen(int(t[1:]), r-ntc[t]*tspace)
@@ -67,7 +85,6 @@ for i in range (nodes_count):
     f.write("    <place displayName=\"true\" id=\"" + nodes_label[nodes[i]] + "\" initialMarking=\"0\" invariant=\"&lt; inf\" name=\"" + nodes_label[nodes[i]] + "\" nameOffsetX=\"-5.0\" nameOffsetY=\"35.0\" positionX=\"" + str(x) + "\" positionY=\"" + str(y) + "\"/>\n")
 
 #### PART II: TRANSITIONS
-
 for i in range (edges_count):
     source, target = transitions[i]
     x0, y0 = nodes_cords[source]
@@ -75,7 +92,6 @@ for i in range (edges_count):
     f.write("    <transition angle=\"" + str(slope(x0, xs, y0, ys)) + "\" displayName=\"true\" id=\"" + "T" + str(i) + "\" infiniteServer=\"false\" name=\"" + "T" + str(i) + "\" nameOffsetX=\"-5.0\" nameOffsetY=\"35.0\" positionX=\"" + str(x) + "\" positionY=\"" + str(y) + "\" priority=\"0\" urgent=\"false\"/>\n")
 
 ##### PART III: ARCS
-#Generating node-transition arcs, idem to previous
 for i in range(edges_count):
     source, target = transitions[i]
     print(source + " to " + target + " through " + transitions_label[(source, target)])
